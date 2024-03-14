9bc3e8252d5113914f09e205c90dc78f42f2ade5
mehmet.kara <mehmet.kara@astron.com.tr>
2024-03-14 17:13:22 +0300

    Initial commit

---
+++ router/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/About" element={<About></About>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
